92bc711eee1bd6a7f3960d5d143bc2a6ea8cfd35
Lok-yin Tang <lotang@Lok-yins-MacBook-Pro.local>
2021-08-03 10:30:11 -0700

    changed dataset from preprocessed tensors to raw csv files

--- oddl/main.py
+++ oddl/main.py
@@ -21,12 +21,13 @@ from torch.autograd import Variable
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
 
 from models import *
+from command_dataset import CommandDataset
 
-DATA_DIR = '../data/processed_tensors/'
+DATA_DIR = '../data/processed_csv/'
 SCRIPTS_DIR = '../data/scripts/'
 PREP_DIR = '../data/prep/'
 EPOCHS = 100
-BATCH_SIZE = 8
+BATCH_SIZE = 64
 
 model = ShallowCNN()
 model_file = 'models/cnn-shallow-conv-1-fc-2048-1024.pth'
@@ -34,17 +35,6 @@ cuda_device = 0
 
 # set random seed for reproducibility
 torch.manual_seed(42)
-
-class ScriptDataset(torch.utils.data.Dataset):
-    def __init__(self, data):
-        self.x = data['x']
-        self.y = data['y']
-    
-    def __len__(self):
-        return len(self.y)
-
-    def __getitem__(self, index):
-        return self.x[index], self.y[index]
 
 parser = argparse.ArgumentParser(description='obfuscation detection train file')
 parser.add_argument('--reset', help='start over training', action='store_true')
@@ -122,8 +112,8 @@ mse = nn.MSELoss()
 epoch = 0
 
 # load data
-train_data = ScriptDataset(torch.load(DATA_DIR + 'train_data.pth'))
-val_data = ScriptDataset(torch.load(DATA_DIR + 'val_data.pth'))
+train_data = CommandDataset(DATA_DIR + 'train-data.csv')
+val_data = CommandDataset(DATA_DIR + 'val-data.csv')
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
 val_loader = torch.utils.data.DataLoader(val_data, batch_size=BATCH_SIZE, shuffle=False)
 print('loaded data:', len(train_data), len(val_data))
@@ -181,40 +171,15 @@ if args.eval:
     eval_model('val', model, val_loader, len(val_data), mse)
 elif args.test:
     # eval model on test
-    test_data = ScriptDataset(torch.load(DATA_DIR + 'test_data.pth'))
+    test_data = CommandDataset(torch.load(DATA_DIR + 'test_data.pth'))
     test_loader = torch.utils.data.DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False)
     eval_model('test', model, test_loader, len(test_data), mse)
-elif args.analyze:
-    def print_command(script_tensor, int_to_char_dict, ffile):
-        script = ''
-        if not (args.model.startswith('lstm') or args.model.startswith('resnet')):
-            script_tensor = script_tensor[0].T
-        script_tensor_idx, script_tensor = torch.nonzero(script_tensor, as_tuple=True)
-        for i in range(script_tensor.shape[0]):
-            if i < script_tensor.shape[0] - 1 and script_tensor_idx[i] == script_tensor_idx[i + 1]:
-                script += int_to_char_dict[int(script_tensor[i])].upper()
-            elif int(script_tensor[i]) != 73:
-                script += int_to_char_dict[int(script_tensor[i])]
-        ffile.write(script)
-
+elif args.analyze:        
     model.eval()
-    # eval_data = ScriptDataset(torch.load(DATA_DIR + 'dos_data.pth'))
-    # eval_data = ScriptDataset(torch.load(DATA_DIR + 'hubble_data.pth'))
-    # eval_data = ScriptDataset(torch.load(DATA_DIR + 'cb_data.pth'))
-    # eval_data = ScriptDataset(torch.load(DATA_DIR + 'cb2-2_data.pth'))
+    CSV_FILE = 'val-data.csv'
+    eval_csv = pd.read_csv(DATA_DIR + CSV_FILE)
+    # eval_data = CommandDataset(DATA_DIR + CSV_FILE)
     # eval_loader = torch.utils.data.DataLoader(eval_data, batch_size=BATCH_SIZE, shuffle=False)
-    # filenames = torch.load('../data/scripts/val_cmds_list.pth')
-    commands = torch.load('../data/scripts/val_cmds_list.pth')
-    # commands = torch.load(SCRIPTS_DIR + 'ps_scripts.pth')
-    # commands = torch.load(SCRIPTS_DIR + 'dos_cmds.pth')
-    # commands = torch.load(SCRIPTS_DIR + 'hubble_cmds.pth')
-    # commands = torch.load(SCRIPTS_DIR + 'cb_cmds.pth')
-    # commands = torch.load(SCRIPTS_DIR + 'cb2-2_cmds.pth')
-    char_dict = torch.load(PREP_DIR + 'char_dict.pth')
-    int_to_char_dict = {}
-    for char in char_dict:
-        int_to_char_dict[char_dict[char]] = char
-    print(int_to_char_dict)
     fn_file = open('fn.txt', 'w')
     fp_file = open('fp.txt', 'w')
     tp_file = open('tp.txt', 'w')
@@ -222,7 +187,6 @@ elif args.analyze:
 
     # print(len(eval_loader))
     # print(len(eval_data))
-    print(len(commands))
 
     # analyze on all val samples
     y_true = []
@@ -242,36 +206,24 @@ elif args.analyze:
             # print(i, j)
             # false negatives
             if curr_y_pred[j] == 0 and curr_y_true[j] == 1:
-                # fn_file.write('\n{:s}\n'.format(filenames[curr_idx]))
-                # fn_file.write('Script {:d}\n'.format(curr_idx))
-                # print_command(data[j], int_to_char_dict, fn_file)
-                
-                # fn_file.write('\nScript {:d}\n'.format(curr_idx))
-                # fn_file.write(commands[curr_idx])
+                fn_file.write('\nScript {:d}\n'.format(curr_idx))
+                fn_file.write(eval_csv.loc[curr_idx]['command'])
             
-                fn_file.write('"{:s}"\n'.format(commands[curr_idx]))
+                # fn_file.write('"{:s}"\n'.format(commands[curr_idx]))
             
             # false positives
-            if curr_y_pred[j] == 1 and curr_y_true[j] == 0:
-                # fp_file.write('\n{:s}\n'.format(filenames[curr_idx]))
-                # fp_file.write('Script {:d}\n'.format(curr_idx))
-                # print_command(data[j], int_to_char_dict, fp_file)
+            if curr_y_pred[j] == 1 and curr_y_true[j] == 0:                
+                fp_file.write('\nScript {:d}\n'.format(curr_idx))
+                fp_file.write(eval_csv.loc[curr_idx]['command'])
                 
-                # fp_file.write('\nScript {:d}\n'.format(curr_idx))
-                # fp_file.write(commands[curr_idx])
-                
-                fp_file.write('"{:s}"\n'.format(commands[curr_idx]))
+                # fp_file.write('"{:s}"\n'.format(commands[curr_idx]))
 
             # true positives
-            if curr_y_pred[j] == 1 and curr_y_true[j] == 1:
-                # tp_file.write('\n{:s}\n'.format(filenames[curr_idx]))
-                # tp_file.write('Script {:d}\n'.format(curr_idx))
-                # print_command(data[j], int_to_char_dict, tp_file)
-                
-                # tp_file.write('\nScript {:d}\n'.format(curr_idx))
-                # tp_file.write(commands[curr_idx])
+            if curr_y_pred[j] == 1 and curr_y_true[j] == 1:                
+                tp_file.write('\nScript {:d}\n'.format(curr_idx))
+                tp_file.write(eval_csv.loc[curr_idx]['command'])
 
-                tp_file.write('"{:s}"\n'.format(commands[curr_idx]))
+                # tp_file.write('"{:s}"\n'.format(commands[curr_idx]))
 
         y_pred += curr_y_pred
         y_true += curr_y_true
@@ -318,65 +270,6 @@ elif args.analyze:
     print('\trecall: {:f}'.format(recall))
     print('\tconfusion matrix (tn, fp, fn, tp): {:d}, {:d}, {:d}, {:d}'.format(tn, fp, fn, tp))
 elif args.run:
-    '''
-    # run to classify the real-world data points from the dataset
-    real_world_data = ScriptDataset(torch.load(DATA_DIR + 'flip_train_data_real_world.pth'))
-    real_world_loader = torch.utils.data.DataLoader(real_world_data, batch_size=BATCH_SIZE, shuffle=False)
-
-    LABELS_DIR =  '../Revoke-Obfuscation/DataScience/'
-    githubgist_file = pd.read_csv(LABELS_DIR + 'GithubGist-obfuscation-labeledData.csv')
-    poshcode_file = pd.read_csv(LABELS_DIR + 'PoshCode-obfuscation-labeledData.csv')
-    technet_file = pd.read_csv(LABELS_DIR + 'TechNet-obfuscation-labeledData.csv')
-    real_world_filenames = torch.load('filenames_real_world.pth')
-
-    new_githubgist_file = open('new-GithubGist-obfuscation-labeledData.csv', 'w')
-    new_poshcode_file = open('new-PoshCode-obfuscation-labeledData.csv', 'w')
-    new_technet_file = open('new-TechNet-obfuscation-labeledData.csv', 'w')
-
-    def write_negatives(old_file, new_file):
-        new_file.write('"Path","Label"\n')
-        for _, row in old_file.iterrows():
-            if int(row[1]) == 0:
-                new_file.write('"{:s}","0"\n'.format(row[0]))
-    
-    def write_label(ffile, filename, label):
-        filename = filename.replace('/', '\\')
-        ffile.write('"{:s}","{:d}"\n'.format(filename, label))
-
-    write_negatives(githubgist_file, new_githubgist_file)
-    write_negatives(poshcode_file, new_poshcode_file)
-    write_negatives(technet_file, new_technet_file)
-
-    model.eval()
-    preds = []
-    for _, (data, label) in enumerate(real_world_loader):
-        data, label = Variable(data.type(torch.float).to(device)), \
-                            Variable(label.type(torch.float).to(device))
-        output = model(data)
-        preds += list(torch.max(output, dim=1).indices.cpu().numpy())
-    
-    pred1s = 0
-    for i in range(len(preds)):
-        filename = real_world_filenames[i]
-        pred = preds[i]
-        if filename.startswith('GithubGist'):
-            write_label(new_githubgist_file, filename, pred)
-        elif filename.startswith('PoshCode'):
-            write_label(new_poshcode_file, filename, pred)
-        elif filename.startswith('TechNet'):
-            write_label(new_technet_file, filename, pred)
-        else:
-            print('ERROR:', filename)
-        
-        if pred == 1:
-            pred1s += 1
-            print('pred 1 {:d}:'.format(pred1s), filename)
-
-    print('pred 0s:', preds.count(0))
-    print('pred 1s:', preds.count(1))
-    '''
-
-    
     # run classification on test-scripts directory
     TEST_DIR = '../test-scripts/'
     for ffile in os.listdir(TEST_DIR):
@@ -412,10 +305,14 @@ elif args.run:
             print(e)
 else:
     # train model
+    print(len(train_loader))
     for i in range(epoch, EPOCHS):
         print('epoch', i)
         # run training
         model.train()
+        avg_loss = 0.
+        y_true = []
+        y_pred = []
         for batch_idx, (data, label) in enumerate(train_loader):
             data, label = Variable(data.type(torch.float).to(device)), \
                             Variable(label.type(torch.float).to(device))
@@ -425,11 +322,28 @@ else:
             loss.backward()
             optimizer.step()
 
-            if batch_idx % 1000 == 0:
+            avg_loss += loss.data
+            y_pred += list(torch.max(output, dim=1).indices.cpu().numpy())
+            y_true += list(torch.max(label, dim=1).indices.cpu().numpy())
+
+            if batch_idx % int(len(train_loader) / 5) == 0:
                 print('\t\tbatch {:d}: {:f}'.format(batch_idx, loss.data))
 
         # eval model
-        eval_model('train', model, train_loader, len(train_data), mse)
+        # eval_model('train', model, train_loader, len(train_data), mse)
+        avg_loss /= len(train_loader)
+        acc = accuracy_score(y_true, y_pred)
+        f1 = f1_score(y_true, y_pred)
+        prec = precision_score(y_true, y_pred)
+        recall = recall_score(y_true, y_pred)
+        tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+        print('\ttrain loss: {:f}'.format(avg_loss))
+        print('\ttrain accuracy: {:f}'.format(acc))
+        print('\ttrain f1: {:f}'.format(f1))
+        print('\ttrain precision: {:f}'.format(prec))
+        print('\ttrain recall: {:f}'.format(recall))
+        print('\ttrain confusion matrix (tn, fp, fn, tp): {:d}, {:d}, {:d}, {:d}'.format(tn, fp, fn, tp))
+
         _, val_f1 = eval_model('val', model, val_loader, len(val_data), mse)
 
         # save model every epoch if better val f1
